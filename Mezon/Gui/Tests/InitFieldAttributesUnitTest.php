bb84b597878e5d184b963a1e35b46922f88119bb
gdever <alexey@dodonov.pro>
2022-04-11 17:05:27 +0300

    feature/Unit-test was added

--- Mezon/Gui/Tests/InitFieldAttributesUnitTest.php
+++ Mezon/Gui/Tests/InitFieldAttributesUnitTest.php
@@ -29,4 +29,20 @@ class InitFieldAttributesUnitTest extends TestCase
         $this->assertFalse($fieldAtributes->isCustom());
         $this->assertFalse($fieldAtributes->isBatch());
     }
+
+    /**
+     * Testing exception when field name is not set
+     */
+    public function testExceptionNoName(): void
+    {
+        // assertions
+        $this->expectException(\Exception::class);
+        $this->expectExceptionCode(- 1);
+        $this->expectExceptionMessage('Name of the field is not defined');
+
+        // setup and test body
+        new FieldAttributes([
+            'type' => 'common'
+        ]);
+    }
 }
